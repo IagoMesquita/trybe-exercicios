40d6fdcce51130b7eb048b2cdd50919d2146197a
Iago Mesquita <iagomesquitaalves@gmail.com>
2021-07-28 19:05:11 -0300

    Adciona exercicios com operadores matematicos

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scriptsOMat.js
@@ -0,0 +1,16 @@
+//1 - Cinco programas, um para cada operação aritmética básica.//
+
+let a = 15;
+let b = 21;
+
+let adição = a + b;
+let subtração = a - b;
+let multiplicação = a * b;
+let divisão = a / b;
+let módulo = b % a;
+
+console.log(adição);
+console.log(subtração);
+console.log(multiplicação);
+console.log(divisão);
+console.log(módulo);
