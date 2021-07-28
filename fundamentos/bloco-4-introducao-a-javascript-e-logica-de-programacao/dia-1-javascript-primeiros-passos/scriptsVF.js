2769cbf366b9fd2ab5d6ad35a1f4ca70bb28305e
Iago Mesquita <iagomesquitaalves@gmail.com>
2021-07-28 20:42:35 -0300

    Adiciona HEADME bloco 4 e EXC 2-3 compara valores.

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scriptsVF.js
@@ -0,0 +1,27 @@
+//Exercicio 2//
+
+let x = 101;
+let y = 70;
+
+if (x > y){
+    console.log(x);
+}
+else {
+    console.log(y);
+}
+
+//Exercicio 3//
+
+let a = 42;
+let b = 55;
+let c = 59;
+
+if (a > b && a > c){
+    console.log(a);
+}
+else if (b > a && b > c){
+    console.log(b);
+}
+else {
+    console.log(c);
+}
