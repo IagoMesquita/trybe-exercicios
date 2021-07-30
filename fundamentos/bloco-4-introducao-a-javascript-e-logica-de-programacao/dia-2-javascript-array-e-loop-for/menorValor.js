db07499b5ec1bbd85fac9873eec5f105cf7bbfc8
Iago Mesquita <iagomesquitaalves@gmail.com>
2021-07-30 13:21:12 -0300

    Adiciona exercicio 7, menor valor array.

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/menorValor.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maior = 0;
+let menor = numbers[0];
+
+for (let i = 0; i < numbers.length; i +=1 ){
+    if(numbers[i] < menor){
+      menor = numbers[i]  
+    }
+}
+
+console.log("Menor valor do array é: " + menor );
