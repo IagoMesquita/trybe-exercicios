3cc05191e06a66b91239cc79d951a0edbd3917f3
Iago Mesquita <iagomesquitaalves@gmail.com>
2021-07-30 09:30:26 -0300

    Adiciona exercicio 4, Med Array.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scriptsMedArr.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scriptsMedArr.js
@@ -3,5 +3,12 @@ soma = 0;
 
 for(let i = 0; i < numbers.length; i += 1){
     soma += numbers[i]
+    }
+let med = soma/numbers.length 
+
+if (med > 20){
+    console.log("Valor maior que 20.");
+}   
+else{
+    console.log("Valor menor ou igual a 20.")
 }
-console.log((soma)/(numbers.length));
