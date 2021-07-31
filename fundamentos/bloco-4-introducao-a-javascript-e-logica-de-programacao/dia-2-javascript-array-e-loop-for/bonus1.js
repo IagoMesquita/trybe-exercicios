6265ff1d2f024ff89ce4f50f1ab111d0161f02f6
Iago Mesquita <iagomesquitaalves@gmail.com>
2021-07-31 10:37:48 -0300

    Atualiza para versao correta.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bonus1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bonus1.js
@@ -2,11 +2,27 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (let index = 1; index < numbers.length; index += 1) {
     for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-      if (numbers[index] < numbers[secondIndex]) {
-        let position = numbers[index];
-        numbers[index] = numbers[secondIndex];
-        numbers[secondIndex] = position;
-      }
+      if (numbers[secondIndex] > numbers[index]) {        //<-- inverti a ordem para e troquei  
+        let position = numbers[secondIndex];              //o sinal pra não me confundir;.
+        numbers[secondIndex] = numbers[index];           //<-- No position atribui a posição 0      
+        numbers[index] = position;                       //e consequentemente invertei todo o resto.
+       }
     }
   }
   console.log(numbers)
+
+
+//Artigo DevFuria 
+// // Vamos ter que percorrer todo o vetor, logo:
+// for (k = 1; k < n; k++) {
+//     // Dentro de cada iteração
+//     // percorremos novamente o vetor
+//     // em busca dos pares
+//     for (j = 0; j < n - k; j++) {
+//         // Comparamos
+//         if (vetor[j] > vetor[j + 1]) {
+//             // swap (trocamos)
+//             aux          = vetor[j];
+//             vetor[j]     = vetor[j + 1];
+//             vetor[j + 1] = aux;
+//         }
