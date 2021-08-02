c9d8db5c380957b1ce42318dbc3c43c9cb733c85
Iago Mesquita <iagomesquitaalves@gmail.com>
2021-08-02 10:54:24 -0300

    Adiciona exercicio 1, asterisco 5x5

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/asteriscosN5.js
@@ -0,0 +1,34 @@
+/*MEU CÓDIGO*/
+
+let n = 5;
+
+for(i = 1; i <= 5; i += 1){
+        console.log(); 
+        console.log("*****");
+}
+
+// Meu código é menor, porem é menos assertivo. Pois deixa um
+// espaçamento grade entre as linhas, o que não seria interessante,
+// por exemplo, para a criação de uma imagem.
+
+
+// ---------------------------------------------------------------------------
+
+//TRYBE CÓDIGO
+
+// let n = 5;
+// let symbol = '*';
+// let inputLine = '';
+
+// for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+//   inputLine = inputLine + symbol;
+// };
+// for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+//   console.log(inputLine);
+// };
+
+
+
+
+
+
