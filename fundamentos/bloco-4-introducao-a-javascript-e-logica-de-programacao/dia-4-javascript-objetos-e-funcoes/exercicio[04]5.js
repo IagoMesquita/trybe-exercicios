f2642774cbfccf6482a260a7542c001efcf3eb47
Iago Mesquita <iagomesquitaalves@gmail.com>
2021-08-02 15:56:10 -0300

    Adiciona exercicio 5, altera e exibe os elementos da chave.

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio[04]5.js
@@ -0,0 +1,9 @@
+let info = {
+    personagem: 'Margarida e Tio Patinhas',
+    origem: "Pato Dolnad e Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Dolnad e O último MacPatinhas',
+    recorrente: 'Ambos recorrentes'
+};
+for (let personagens in info){
+    console.log(info[personagens]);
+}
