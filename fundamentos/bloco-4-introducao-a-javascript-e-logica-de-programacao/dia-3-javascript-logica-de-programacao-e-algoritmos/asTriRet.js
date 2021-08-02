4e4fb99b15f43b9e3c97547798133969b4f60861
Iago Mesquita <iagomesquitaalves@gmail.com>
2021-08-02 13:06:30 -0300

    Adiciona melhor estilo ao triangulo

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/asTriRet.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/asTriRet.js
@@ -6,3 +6,18 @@ for(i = 1; i <= 5; i += 1){
     linAst = linAst + ast
     console.log(linAst);   
 }
+
+//Triângulo com mais espçamento.
+//Tentando fazer a questão 3, consegui dar 
+//mais epaço entre os asterisco.
+
+let n = 5;
+let ast = '*';
+let linAst = '';
+
+for(let i = 1; i <= 5; i += 1){
+    linAst = linAst + ast 
+    
+    linAst = linAst + " "  
+    console.log(linAst);  
+}
