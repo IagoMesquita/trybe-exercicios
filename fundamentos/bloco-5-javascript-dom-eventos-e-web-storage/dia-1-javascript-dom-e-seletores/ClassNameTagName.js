23f179feeb82cf3d03e24baf07afce1d9a0e2644
Iago Mesquita <iagomesquitaalves@gmail.com>
2021-08-10 08:41:33 -0300

    Adiciona exercicio de fixação 2, getElementsClassName e getElementsTagName.

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/ClassNameTagName.js
@@ -0,0 +1,18 @@
+
+
+let filmes = document.getElementsByClassName("paragraph");
+for (let i = 0; i < filmes.length; i += 1){
+    filmes[i].innerText = "Matrix & Harry Potter";
+}
+
+let paragrafos = document.getElementsByClassName("paragraph");
+
+paragrafos[0].style.background = 'red';
+paragrafos[0].style.color = 'green';    
+paragrafos[0].style.fontSize = '3.75rem';
+paragrafos[0].style.margin =  
+document.getElementById("subtitle").innerText = "Só os melhores!";
+let titulo = document.getElementsByTagName("h2");
+titulo[0].innerText = "FILMES";
+titulo[0].style.fontSize = '2.5rem';
+    
