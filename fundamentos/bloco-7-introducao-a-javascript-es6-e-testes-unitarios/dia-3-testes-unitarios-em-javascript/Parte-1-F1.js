a8e6f0271a11270a3879caf4f5ddd0e4ffbac0b3
Iago Mesquita <iagomesquitaalves@gmail.com>
2021-09-01 11:56:38 -0300

    Adiciona Exercici 1, Parte 1. De acordo com o Gabarito

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/Parte-1-F1.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/Parte-1-F1.js
@@ -7,7 +7,15 @@ function sum(a, b){
     return a + b;
 }
 
-const expected = sum('4', 5);
+const expected = sum(4, 5);
 
+assert.strictEqual(typeof sum, 'function');
 assert.strictEqual(expected, 9, "A soma de 4 + 5 deve ser 9");
+assert.strictEqual(sum(4, 5), 9);
 assert.strictEqual(sum(0, 0), 0, "A soma de zeros deve ser 0");
+assert.throws(() => {
+    sum(4, '5');
+});
+assert.throws(() => {
+    sum(4, '5');
+}, /^Error: parameters must be numbers$/)
