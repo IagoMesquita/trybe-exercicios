4cf125397b0ac6c022eeec0f0543d5eec00c2a4d
Iago Mesquita <iagomesquitaalves@gmail.com>
2021-09-02 20:51:57 -0300

    Refatora exercicio-1,trata e-mail

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicio-1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicio-1.js
@@ -1,4 +1,4 @@
-const newEmployees = (callBack) => {
+const newEmployees = () => {
     const employees = {
       id1: newPerson('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
       id2: newPerson('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
@@ -8,12 +8,14 @@ const newEmployees = (callBack) => {
   };
 
 function newPerson(nome) {
-const info = {
-nome,
-email: `${nome}@betrybe.com`,
-}
-return info;
+  const email = nome.toLowerCase().split(' ').join('_'); 
+  return {nome, email:`${email}@betrybe.com`}
+// const info = {
+// nome,
+// email: `${nome}@betrybe.com`,
+// }
+// return info;
 };
 
-console.log(newEmployees(newPerson()));
+console.log(newEmployees());
 
