9f38836beeece4cd42b59dfa41929446db137fd0
Iago Mesquita <iagomesquitaalves@gmail.com>
2021-09-13 10:09:10 -0300

    Adiciona Gabarito exercicio5, foi utilizado reduce no reduce

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicio5.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicio5.js
@@ -11,20 +11,31 @@ const names = [
   "Adieidy",
   "Alarucha",
 ];
-// Conta a quantidade de letras 'A' e 'a'. No meu código utileixei além do reduce o forEach.
+//Gabarito Trybe: Usou menos linhas tutilizando apenas reduce dentro de reduce.
 function containsA() {
-  const letras = names.reduce((acc, letras) => {
-    return acc.concat(letras.split(""));
-  }, []);
-  let letraA = 0;
-  const countA = letras.forEach((letra) => {
-    if (letra === "A" || letra === "a") {
-      return (letraA += 1);
-    }
-  });
-  return letraA;
-}
+    return names.reduce((acc, curr) =>
+       acc + curr.split('').reduce((acumulator, current) => {
+          if (current === 'a' || current === 'A') return acumulator + 1;
+          return acumulator;
+       }, 0), 0);
+  }
+  
+  assert.deepStrictEqual(containsA(), 20);
+  
+// Conta a quantidade de letras 'A' e 'a'. No meu código utileixei além do reduce o forEach.
+// function containsA() {
+//   const letras = names.reduce((acc, letras) => {
+//     return acc.concat(letras.split(""));
+//   }, []);
+//   let letraA = 0;
+//   const countA = letras.forEach((letra) => {
+//     if (letra === "A" || letra === "a") {
+//       return (letraA += 1);
+//     }
+//   });
+//   return letraA;
+// }
 
-assert.deepStrictEqual(containsA(), 20);
+// assert.deepStrictEqual(containsA(), 20);
 
-console.log(containsA());
+// console.log(containsA());
