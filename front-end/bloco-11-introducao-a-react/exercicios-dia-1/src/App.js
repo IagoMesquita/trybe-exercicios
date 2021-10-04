9198e62b7c368778dd6f76ecaa862dbf762d3fb6
Iago Mesquita <iagomesquitaalves@gmail.com>
2021-10-04 19:10:01 -0300

    Exercicio bloco 11.1, lista de Hello, com funcao map

--- front-end/bloco-11-introducao-a-react/exercicios-dia-1/src/App.js
+++ front-end/bloco-11-introducao-a-react/exercicios-dia-1/src/App.js
@@ -1,21 +1,19 @@
+import React from 'react';
 import './App.css';
 
+const arrayHello = ['Hello, World!', 'Hello, Trybe!', 'Hello, Iago'];
 
 function App() {
-  const Task = (value) => {
-    return (
-     <div> 
-      <li>{'Hello, World!!'}</li>
-      <li>{'Hello, Trybe'}</li>
-      <li>{'Hello, Iago'}</li>
-    </div>  
-    );
-  }
-  return (
-    <div className="App">
-      <Task/>
-    </div>
-  );
+const Task = (value) => {
+return (
+<li>{value}</li>
+);
+}
+return (
+<div className="App">
+{arrayHello.map((hello) => Task(hello) )} 
+</div>
+);
 }
 
 export default App;
